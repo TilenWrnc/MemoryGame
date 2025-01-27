f82d6316a7eeeedf61deeb035387fa149a536fca
Tilen <tilen.bratina3@gmail.com>
2025-01-27 17:36:26 +0100

    Final Commit

--- memoryGame/src/App.jsx
+++ memoryGame/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,11 +6,47 @@ import "./components.jsx"
 import { DashBoard, Main } from './components.jsx'
 
 function App() {
+    const [gameStarted, setGameStarted] = useState(false);
+    const [gameWin, setGameWin] = useState(false);
+
+    function handleGameStart() {
+        setGameStarted(!gameStarted);
+    }
+
+    const [score, setScore] = useState(0);
+
+    function handleScore() {
+        setScore(score + 1);
+    }
+
+    useEffect(() => {
+        if (score === 14) {
+            setGameWin(true);
+        }
+    }, [score]);
+
+    function resetScore() {
+        setScore(0)
+    }
+
+
     return (
-        <>
-            <DashBoard/>
-            <Main/>
-        </>
+        <div className='main'>
+            <DashBoard 
+                gameStarted = {gameStarted} 
+                setGameStarted = {handleGameStart}
+                score = {score}
+                setScore = {handleScore}
+            />
+            <Main 
+                gameStarted = {gameStarted} 
+                setGameStarted = {handleGameStart}
+                score = {score}
+                setScore = {handleScore}
+                resetScore = {resetScore}
+                gameWin = {gameWin}
+            />
+        </div>
     )
 
 }
--- memoryGame/src/components.jsx
+++ memoryGame/src/components.jsx
@@ -1,60 +1,148 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import "./components.css"
 
-function DashBoard() {
+function DashBoard( {gameStarted, setGameStarted, score, setScore} ) {
+    function resetGame () {
+        if (gameStarted) {
+            setScore(0);
+            setGameStarted();
+        }
+    }
+
     return (
         <div className="dashboard">
             <div className="dashboard-left">
-                <h1>MEMORY GAME</h1>
+                <h1 onClick={resetGame}>MEMORY GAME</h1>
             </div>
             <div className="dashboard-right">
-                <h2>SCORE: </h2>
-                <h2>HIGH SCORE: </h2>
+                <h2>SCORE: {score} / 14</h2>
             </div>
         </div>
     )
 }
 
-function Main() {
-    const [gameStarted, setGameStarted] = useState(false);
+function Main( {gameStarted, setGameStarted, setScore, resetScore, gameWin} ) {
 
-    function handleGameStart() {
-        setGameStarted(true);
-        setGameboard();
+    const [fetchedPokemonList, setFetchedPokemonList] = useState([]);
+    const [pokemonList, setPokemonList] = useState([]);
+    const [round, setRound] = useState(0);
+    const [gameLost, setGameLost] = useState(false);
+    const [gameRestarted, setGameRestarted] = useState(false);
+
+    function handleGameRestart() {
+        setGameRestarted(!gameRestarted);
+        setGameLost(!gameLost);
+        resetScore();
     }
 
     function getRandomNumber() {
         return Math.floor(Math.random() * 100) + 1;
-      }
-
-    async function setGameboard() {
-        const pokemonArray = []
-        for (let i = 0; i < 14; i++) {
-            const setArray = new Set();
-            let randomPokemonSelect = getRandomNumber();
-            setArray.add(random)
-            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonSelect}/`);
-            let pokemonSelect = await response.json();
-            console.log(pokemonSelect);
-
-        }
     }
-    
+
+    function changeRound(pokemonID) {
+        shufflePokemonList();
+        setRound(round + 1);
+        checkCorrectCard(pokemonID)
+    }
+
+    function checkCorrectCard(pokemonID) {
+        setFetchedPokemonList((prevList) => {
+            console.log(fetchedPokemonList[pokemonID])
+            const updatedList = [...prevList];
+            if (!updatedList[pokemonID].clicked) {
+                setTimeout(() => {
+                    setScore(); 
+                }, 0); 
+                updatedList[pokemonID] = { 
+                    ...updatedList[pokemonID], 
+                    clicked: true,             
+                };
+            } else {
+                handleGameLose(); 
+            }
+            return updatedList; 
+        });
+    }
+
+    function handleGameLose() {
+        setGameLost(!gameLost);
+    }
+
+    function shuffleArray(array) {
+        for (let i = array.length - 1; i > 0; i--) {
+          const j = Math.floor(Math.random() * (i + 1)); 
+          [array[i], array[j]] = [array[j], array[i]];  
+        }
+        return array;
+    }
+
+    function shufflePokemonList() {
+        const shuffledArray = shuffleArray([...fetchedPokemonList]);
+        setPokemonList(shuffledArray);
+    }
+
+    useEffect(() => {
+        const fetchPokemon = async () => {
+            const pokemonArray = []
+            const setArray = new Set();
+            for (let i = 1; i < 15; i++) {
+                let randomPokemonId = null;
+                while (setArray.size !== i) {
+                    randomPokemonId = getRandomNumber();
+                    setArray.add(randomPokemonId)
+                }
+                    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomPokemonId}/`);
+                    let pokemonSelect = await response.json();
+                    let pokemonObject = {
+                        name: pokemonSelect.name,
+                        imgUrl: pokemonSelect.sprites["front_default"],
+                        Id: i - 1,
+                        clicked: false,
+                    }
+                pokemonArray.push(pokemonObject)            
+            }
+            setFetchedPokemonList(pokemonArray);
+            setPokemonList(pokemonArray)
+        };
+        fetchPokemon();
+    }, [gameRestarted])
+ 
     return (
         <>
-            {!gameStarted && (
+            {!gameStarted && !gameLost && !gameWin && (
                 <div className="button-container">
-                    <button className="start-game-button" onClick={handleGameStart}>START GAME</button>
+                    <button className="start-game-button" onClick={setGameStarted}>START GAME</button>
                 </div>
             )}
-            {gameStarted && (
+
+            {gameStarted && !gameLost && !gameWin && (
                 <div className="card-container">
-                    
+                    {pokemonList.map((pokemon) => (
+                        <div className="card" key={pokemon.Id} onClick={() => changeRound(pokemon.Id)}>
+                            <h3 className="pokemon-name">{pokemon.name}</h3>
+                            <img className="pokemon-image" src={pokemon.imgUrl}></img>
+                        </div>
+                    ))}
                 </div>
+            )}
+            {gameLost  && !gameWin && (
+                <div className="lose-message">
+                    <p>{"You Lose"}</p>
+                    <button className="restart-button" onClick={handleGameRestart}>RESTART GAME</button>
+                </div>
+            )}
+
+            {gameWin && (
+                <div className="win-message">
+                   <p>{"You Win"}</p>
+               </div>
             )}
         </>
     )
 
 }
+
+
+
 
 export {DashBoard, Main}
